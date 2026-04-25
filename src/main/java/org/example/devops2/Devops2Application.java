a1d36b8f82338dbd94b6806447e9706543dd8b42
Fatma <fatmaceylin2004@gmail.com>
2026-04-25 11:50:29 +0300

    deniyorum

--- src/main/java/org/example/devops2/Devops2Application.java
+++ src/main/java/org/example/devops2/Devops2Application.java
@@ -2,15 +2,9 @@ package org.example.devops2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Devops2Application {
     public static void main(String[] args) {
         SpringApplication.run(Devops2Application.class, args);
