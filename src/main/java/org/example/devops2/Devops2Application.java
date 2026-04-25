76a4037441ce850247f06a9ebf382637af3153ea
Fatma <fatmaceylin2004@gmail.com>
2026-04-25 11:27:43 +0300

    veritabanı sorunu

--- src/main/java/org/example/devops2/Devops2Application.java
+++ src/main/java/org/example/devops2/Devops2Application.java
@@ -2,12 +2,11 @@ package org.example.devops2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Devops2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Devops2Application.class, args);
     }
-
 }
