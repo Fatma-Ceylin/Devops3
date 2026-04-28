fc439cdc343df3c006cf1f9788b996d12a027226
Fatma <fatmaceylin2004@gmail.com>
2026-04-28 08:12:29 +0300

    test 5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#clean the old container , pull the new one
-docker pull ${{ secrets.DOCKERHUB_USERNAME }}/devops3-app:latest
-docker stop my-app-container || true
-docker rm my-app-container || true
-docker run -d --name my-app-container -p 80:8080 ${{ secrets.DOCKERHUB_USERNAME }}/devops3-app:latest
+
+# Kullanıcı adını buraya açıkça yaz (Örn: fceylin)
+IMAGE_NAME="fceylin/devops3-app:latest"
+CONTAINER_NAME="app-webapp-1"
+
+echo "Yeni imaj çekiliyor: $IMAGE_NAME"
+docker pull $IMAGE_NAME
+
+echo "Eski konteyner durduruluyor..."
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+echo "Yeni konteyner başlatılıyor..."
+# Port eşlemesini senin sistemine göre (80:8081) ayarladım
+docker run -d --name $CONTAINER_NAME -p 80:8081 $IMAGE_NAME
