1a35525324b65bba65bbabdb658fa51aef1e72d7
Fatma <fatmaceylin2004@gmail.com>
2026-04-28 08:38:43 +0300

    test 7

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ cd /home/ec2-user/app
 
 echo "--- Yeni imajlar DockerHub'dan çekiliyor ---"
 # Docker compose dosyasındaki imajların (webapp vb.) en güncel halini çeker
-docker compose pull
+docker-compose pull
 
 echo "--- Sistem yeniden başlatılıyor (Database + Uygulama) ---"
 # Eski konteynerleri durdurur ve yenilerini ayağa kaldırır
-docker compose up -d --remove-orphans
+docker-compose up -d --remove-orphans
 
 echo "--- Temizlik yapılıyor ---"
 # Eski/isimsiz imajları silerek sunucuda yer açar
