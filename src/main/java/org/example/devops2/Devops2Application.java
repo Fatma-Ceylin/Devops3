a922a2bd27d289bd2631dac5eec63b7f94f0a132
Fatma <fatmaceylin2004@gmail.com>
2026-04-28 09:16:54 +0300

    hadi bismillah

--- src/main/java/org/example/devops2/Devops2Application.java
+++ src/main/java/org/example/devops2/Devops2Application.java
@@ -2,10 +2,10 @@ package org.example.devops2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Devops2Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Devops2Application.class, args);
     }
